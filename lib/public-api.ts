9a2d67f4b11c7663105a4aab7c1331727649e9ea
卡色 <cipchk@qq.com>
2025-11-25 17:40:41 +0800

    chore: bump angular to v21 (#33)

--- lib/public-api.ts
+++
@@ -1,3 +0,0 @@
-export { HighlightJsDirective } from './src/highlight-js.directive';
-export * from './src/highlight-js.module';
-export * from './src/highlight-js.config';
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideZonelessChangeDetection } from '@angular/core';
 import { App } from './app/app';
 import { provideHighlightJsConfig } from 'ngx-highlight-js';
 
 bootstrapApplication(App, {
-  providers: [provideHighlightJsConfig({ lang: 'html' })],
+  providers: [provideHighlightJsConfig({ lang: 'html' }), provideZonelessChangeDetection()],
 }).catch((err) => console.error(err));
